c813a4943b2664fe684c21cce65bd1bece54d8d3
Arnav Rout <arnav10rout@gmail.com>
2023-09-25 17:25:30 +0530

    style changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,9 +10,9 @@ function Header() {
     </div>
       <nav className='navigation'>
         <ul>
-          <li><a href="/home">Home</a></li>
-          <li><a href="/about">About</a></li>
-          <li><a href="/contact">Contact</a></li>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
         </ul>
       </nav>
     </div>
